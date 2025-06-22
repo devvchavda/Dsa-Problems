75fc5013a5da5457c6a525258614b68be28db29c
devvchavda <devchavda732@gmail.com>
2025-06-22 13:40:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size() ; 
+        int n = t.size() ; 
+        vector<long long> dp(n+1 , 0);
+        dp[n] = 1 ; 
+        for(int i=m-1 ; i>=0 ; i--){
+            for(int j=0 ; j<=n ; j++){
+                if(j==n) dp[j] = 1 ; 
+                else if(s[i]==t[j]){
+                    dp[j] += dp[j+1];
+                }
+            }
+        }
+
+        return dp[0] ; 
+    }
+};
